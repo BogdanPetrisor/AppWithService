8626888a50a51f59978a41d82adfce23957f1514
Petrisor Bogdan <bogdan.petrisor@yopeso.com>
2022-07-15 13:21:47 +0300

    Create stopService() method

--- app/src/main/java/com/example/appwithservice/MyIntentService.kt
+++ app/src/main/java/com/example/appwithservice/MyIntentService.kt
@@ -2,7 +2,7 @@ package com.example.appwithservice
 
 import android.app.IntentService
 import android.content.Intent
-
+import android.util.Log
 
 
 class MyIntentService : IntentService("MyIntentService") {
@@ -10,6 +10,12 @@ class MyIntentService : IntentService("MyIntentService") {
     companion object {
         private lateinit var instance: MyIntentService
         var isRunning = false
+    }
+
+    fun stopService(){
+        Log.d("MyIntentService","Service is stopping...")
+        isRunning = false
+        instance.stopSelf()
     }
 
     override fun onHandleIntent(p0: Intent?) {
